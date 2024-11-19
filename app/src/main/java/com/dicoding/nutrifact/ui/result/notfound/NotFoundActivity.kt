bd738b31c9d7c3addf3ed2119e09b2eab7207765
Rafifsetiawan29 <116824493+Rafifsetiawan29@users.noreply.github.com>
2024-11-19 22:21:43 +0700

    make profile layout

---
+++ app/src/main/java/com/dicoding/nutrifact/ui/result/notfound/NotFoundActivity.kt
@@ -0,0 +1,21 @@
+package com.dicoding.nutrifact.ui.result.notfound
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.nutrifact.R
+
+class NotFoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_not_found)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
